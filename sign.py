378008145d300b38bfc76ded70319ad174433eb4
Lee <77224511+Lizhongbao0221@users.noreply.github.com>
2026-05-24 21:06:44 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,14 +1,41 @@
 import requests
 import os
+import smtplib
+
+from email.mime.text import MIMEText
 
 refresh_token = os.environ.get("ALIYUN_REFRESH_TOKEN")
+
+email_user = os.environ.get("EMAIL_USER")
+email_pass = os.environ.get("EMAIL_PASS")
+email_to = os.environ.get("EMAIL_TO")
 
 headers = {
     "Content-Type": "application/json",
     "User-Agent": "Mozilla/5.0"
 }
 
-# 获取新 token
+def send_email(subject, content):
+
+    msg = MIMEText(content, "plain", "utf-8")
+
+    msg["Subject"] = subject
+    msg["From"] = email_user
+    msg["To"] = email_to
+
+    server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+
+    server.login(email_user, email_pass)
+
+    server.sendmail(
+        email_user,
+        email_to,
+        msg.as_string()
+    )
+
+    server.quit()
+
+# 获取 token
 url = "https://api.aliyundrive.com/v2/account/token"
 
 data = {
@@ -21,19 +48,26 @@ r = requests.post(url, json=data, headers=headers)
 result = r.json()
 
 if "access_token" not in result:
-    print("❌ refresh_token 已失效")
+
+    msg = "❌ 阿里云盘 refresh_token 已失效"
+
+    print(msg)
+
+    send_email(
+        "阿里云盘签到失败",
+        msg
+    )
+
     exit()
 
-# 新 token
+# 新 refresh_token
 new_refresh_token = result["refresh_token"]
 
-# 输出给 GitHub Actions
+# 写回 GitHub ENV
 with open(os.environ['GITHUB_ENV'], 'a') as f:
     f.write(f"NEW_REFRESH_TOKEN={new_refresh_token}\n")
 
 access_token = result["access_token"]
-
-print("✅ access_token 获取成功")
 
 # 签到
 sign_url = "https://member.aliyundrive.com/v1/activity/sign_in_list"
@@ -57,10 +91,33 @@ if sign_result.get("success"):
 
     reward = sign_result["result"]["signInLogs"][count - 1]["reward"]["notice"]
 
-    print(f"✅ 阿里云盘签到成功")
-    print(f"📅 本月累计签到：{count} 天")
-    print(f"🎁 今日奖励：{reward}")
+    msg = f"""
+✅ 阿里云盘签到成功
+
+📅 本月累计签到：{count} 天
+
+🎁 今日奖励：
+{reward}
+"""
+
+    print(msg)
+
+    send_email(
+        "阿里云盘签到成功",
+        msg
+    )
 
 else:
-    print("❌ 签到失败")
-    print(sign_result)
+
+    msg = f"""
+❌ 阿里云盘签到失败
+
+{sign_result}
+"""
+
+    print(msg)
+
+    send_email(
+        "阿里云盘签到失败",
+        msg
+    )
